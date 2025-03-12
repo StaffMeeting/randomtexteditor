c310caefef8118a32d5586825c462c420fd6680f
StaffMeeting <flintwong6@gmail.com>
2025-03-12 13:40:58 +0000

    flexed code

--- src/display.hpp
+++ src/display.hpp
@@ -4,98 +4,99 @@
 #include <algorithm>
 #include "global.h"
 #include <iostream>
-using namespace std;
-string get_filename() {
-	echo();
-	curs_set(1);
 
-	char input[64];
-	getstr(input);
+std::string get_filename() {
+    echo();
+    curs_set(1);
 
-	noecho();
-	curs_set(0);
-	return string(input);
+    char input[69];
+    getstr(input);
+
+    noecho();
+    curs_set(0);
+
+    return std::string(input);
 }
 
 void display() {
-	clear();
-	getmaxyx(stdscr, max_y, max_x);
-	for (int i = 0; i < max_y - 1 && i < content.size(); ++i) {
-		mvprintw(i, 0,"%i", i+1);
-		mvprintw(i, content.size()/10,"|%s", content[i].c_str());
-		if (i == cursor_row) {
-			int draw_col = min(cursor_col, (int)content[i].length()) + content.size()/10;
-			move(i, draw_col);
-			addch(content[i][cursor_col-1] |A_REVERSE);
-		}
-	}
-	refresh();
+    clear();
+    getmaxyx(stdscr, max_y, max_x);
+    for (int i = 0; i < max_y - 1 && i < content.size(); ++i) {
+        mvprintw(i, 0, "%i", i + 1);
+        mvprintw(i, content.size() / 10, "|%s", content[i].c_str());
+        if (i == cursor_row) {
+            int draw_col = std::min(cursor_col, (int)content[i].length()) + content.size() / 10;
+            move(i, draw_col);
+            addch(content[i][cursor_col - 1] | A_REVERSE);
+        }
+    }
+    refresh();
 
-	attron(A_REVERSE);
-	mvprintw(max_y - 1, 0, " Editing: %s| Row: %d Col: %d | F12: Save/Exit\n",filename.c_str(),cursor_row+1, cursor_col);
+    attron(A_REVERSE);
+    mvprintw(max_y - 1, 0, " Editing: %s| Row: %d Col: %d | F12: Save/Exit\n", filename.c_str(), cursor_row + 1, cursor_col);
 
-	clrtoeol();
-	attroff(A_REVERSE);
+    clrtoeol();
+    attroff(A_REVERSE);
 
-	refresh();
+    refresh();
 }
+
 void cmdmode() {
-	while ((ch = getch()) != KEY_F(6)) {
-		switch(tolower(ch)) {
-		case KEY_UP:
-		case 'w': {
-			if (cursor_row > 0) {
-				cursor_row--;
-			}
-			break;
-		}
-		case KEY_LEFT:
-		case 'a': {
-			if (cursor_col > 0) {
-				cursor_col--;
-			}
-			break;
-		}
-		case KEY_DOWN:
-		case 's': {
-			if (cursor_row < content.size() - 1) {
-				cursor_row++;
-			}
-			break;
-		}
-		case KEY_RIGHT:
-		case 'd': {
-			if (cursor_col < content[cursor_row].length()) {
-				cursor_col++;
-			}
-			break;
-		}
-		case 'c': {
-			cmdlist = get_filename();
-			if (cmdlist == "col") {
-				strg = content[cursor_row];
-				display();
-			} else if (cmdlist =="coc") {
-				strg = content[cursor_row][cursor_col-1];
-				display();
-			} else if (cmdlist == "ps") {
-				for (int i=0; i<strg.size(); i++) {
-					char c = strg[i];
-					content[cursor_row].insert(cursor_col,1,c);
-					cursor_col++;
-					display();
-				}
-			} else if (cmdlist =="cul") {
-				strg = content[cursor_row];
-				content[cursor_row].erase();
-				cursor_row++;
-			}
-			break;
-		}
-		}
-		cursor_row = max(0, min(cursor_row, (int)content.size() - 1));
-		cursor_col = max(0, min(cursor_col, (int)content[cursor_row].length()));
-		display();
-	}
+    while ((ch = getch()) != KEY_F(6)) {
+        switch (tolower(ch)) {
+            case KEY_UP:
+            case 'w': {
+                if (cursor_row > 0) {
+                    cursor_row--;
+                }
+                break;
+            }
+            case KEY_LEFT:
+            case 'a': {
+                if (cursor_col > 0) {
+                    cursor_col--;
+                }
+                break;
+            }
+            case KEY_DOWN:
+            case 's': {
+                if (cursor_row < content.size() - 1) {
+                    cursor_row++;
+                }
+                break;
+            }
+            case KEY_RIGHT:
+            case 'd': {
+                if (cursor_col < content[cursor_row].length()) {
+                    cursor_col++;
+                }
+                break;
+            }
+            case 'c': {
+                std::string cmdlist = get_filename();
+                if (cmdlist == "col") {
+                    strg = content[cursor_row];
+                    display();
+                } else if (cmdlist == "coc") {
+                    strg = content[cursor_row][cursor_col - 1];
+                    display();
+                } else if (cmdlist == "ps") {
+                    for (char c : strg) {
+                        content[cursor_row].insert(cursor_col, 1, c);
+                        cursor_col++;
+                        display();
+                    }
+                } else if (cmdlist == "cul") {
+                    strg = content[cursor_row];
+                    content[cursor_row].clear();
+                    cursor_row++;
+                }
+                break;
+            }
+        }
+        cursor_row = std::max(0, std::min(cursor_row, (int)content.size() - 1));
+        cursor_col = std::max(0, std::min(cursor_col, (int)content[cursor_row].length()));
+        display();
+    }
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,101 +3,120 @@
 #include <string>
 #include <fstream>
 #include <algorithm>
+#include <regex>
 #include "display.hpp"
 #include "global.h"
 using namespace std;
 
-int main(int argc, char* argv[]) {
-	content;
-	cursor_row;
-	cursor_col;
-	filename;
+bool is_valid_filename(const std::string& filename) {
+    // Allow only alphanumeric characters, underscores, hyphens, and dots
+    std::regex valid_filename_regex("^[a-zA-Z0-9._-]+$");
+    return std::regex_match(filename, valid_filename_regex);
+}
 
-	initscr();
-	cbreak();
-	noecho();
-	keypad(stdscr, TRUE);
-	curs_set(0);
-	mvprintw(0, 0, "Enter filename:");
-	filename = get_filename();
-	clear();
-	fstream file(filename);
-	if (file.good()) {
-		string line;
-		while (getline(file, line)) content.push_back(line);
-	}
-	if (content.empty()) content.push_back("");
-	display();
-	while ((ch = getch()) != KEY_F(12)) {
-		switch(ch) {
-		case KEY_UP: {
-			if (cursor_row > 0) {
-				cursor_row--;
-			}
-			break;
-		}
-		case KEY_DOWN: {
-			if (cursor_row < content.size() - 1) {
-				cursor_row++;
-			}
-			break;
-		}
-		case KEY_LEFT: {
-			if (cursor_col > 0) {
-				cursor_col--;
-			}
-			break;
-		}
-		case KEY_RIGHT: {
-			if (cursor_col < content[cursor_row].length()) {
-				cursor_col++;
-			}
-			break;
-		}
-		case KEY_ENTER:
-		case '\n':
-		case '\r': {
-			string currline = content[cursor_row];
-			string newline = currline.substr(cursor_col);
-			content[cursor_row] = currline.substr(0, cursor_col);
-			content.insert(content.begin() + cursor_row + 1, newline);
-			cursor_row++;
-			cursor_col = 0;
-			break;
-		}
-		case KEY_BACKSPACE:
-		case 127: {
-			if (cursor_col > 0) {
-				content[cursor_row].erase(cursor_col-1, 1);
-				cursor_col--;
-			} else if (cursor_row > 0) {
-				cursor_col = content[cursor_row - 1].length();
-				content[cursor_row - 1] += content[cursor_row];
-				content.erase(content.begin() + cursor_row);
-				cursor_row--;
-			}
-			break;
-		}
-		case KEY_RESIZE: {
-			break;
-		}
-		case KEY_F(6): {
-			cmdmode();
-			break;
-		}
-		default:
-			if (isprint(ch)) {
-				content[cursor_row].insert(cursor_col, 1, ch);
-				cursor_col++;
-			}
-			break;
-		}
-		cursor_row = max(0, min(cursor_row, (int)content.size() - 1));
-		cursor_col = max(0, min(cursor_col, (int)content[cursor_row].length()));
-		display();
-		ofstream out(filename);
-		for (auto& line : content) out << line << '\n';
-	}
-	endwin();
-	return EXIT_SUCCESS;
+int main(int argc, char* argv[]) {
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    curs_set(0);
+
+    mvprintw(0, 0, "Enter filename:");
+    filename = get_filename();
+    clear();
+
+    if (!is_valid_filename(filename)) {
+        mvprintw(0, 0, "Invalid filename. Only alphanumeric characters, underscores, hyphens, and dots are allowed.");
+        refresh();
+        getch();
+        endwin();
+        return EXIT_FAILURE;
+    }
+
+    std::fstream file(filename);
+    if (file.good()) {
+        std::string line;
+        while (std::getline(file, line)) {
+            content.push_back(line);
+        }
+    }
+    if (content.empty()) {
+        content.push_back("");
+    }
+    display();
+    int ch;
+    while ((ch = getch()) != KEY_F(12)) {
+        switch(ch) {
+        case KEY_UP: {
+            if (cursor_row > 0) {
+                cursor_row--;
+            }
+            break;
+        }
+        case KEY_DOWN: {
+            if (cursor_row < content.size() - 1) {
+                cursor_row++;
+            }
+            break;
+        }
+        case KEY_LEFT: {
+            if (cursor_col > 0) {
+                cursor_col--;
+            }
+            break;
+        }
+        case KEY_RIGHT: {
+            if (cursor_col < content[cursor_row].length()) {
+                cursor_col++;
+            }
+            break;
+        }
+        case KEY_ENTER:
+        case '\n':
+        case '\r': {
+            std::string currline = content[cursor_row];
+            std::string newline = currline.substr(cursor_col);
+            content[cursor_row] = currline.substr(0, cursor_col);
+            content.insert(content.begin() + cursor_row + 1, newline);
+            cursor_row++;
+            cursor_col = 0;
+            break;
+        }
+        case KEY_BACKSPACE:
+        case 127: {
+            if (cursor_col > 0) {
+                content[cursor_row].erase(cursor_col-1, 1);
+                cursor_col--;
+            } else if (cursor_row > 0) {
+                cursor_col = content[cursor_row - 1].length();
+                content[cursor_row - 1] += content[cursor_row];
+                content.erase(content.begin() + cursor_row);
+                cursor_row--;
+            }
+            break;
+        }
+        case KEY_RESIZE: {
+            break;
+        }
+        case KEY_F(6): {
+            cmdmode();
+            break;
+        }
+        default:
+            if (isprint(ch)) {
+                content[cursor_row].insert(cursor_col, 1, ch);
+                cursor_col++;
+            }
+            break;
+        }
+        cursor_row = std::max(0, std::min(cursor_row, (int)content.size() - 1));
+        cursor_col = std::max(0, std::min(cursor_col, (int)content[cursor_row].length()));
+        display();
+        std::ofstream out(filename);
+        for (const auto& line : content) {
+            out << line << '\n';
+        }
+    }
+    endwin();
+    return EXIT_SUCCESS;
 }
