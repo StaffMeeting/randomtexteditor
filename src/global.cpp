7873467bc89c1f621875b002ff0a8903bb039009
StaffMeeting <flintwong6@gmail.com>
2025-03-12 13:02:27 +0800

    Add files via upload

---
+++ src/global.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <string>
+#include "global.h"
+
+int cursor_row = 0;
+int cursor_col = 1;
+std::string filename;
+std::vector<std::string> content;
+std::string cmdlist;
+int ch;
+int max_y;
+int max_x;
+std::string strg;
---
+++ src/global.h
@@ -0,0 +1,10 @@
+#pragma once
+extern int cursor_row;
+extern int cursor_col;
+extern std::string filename;
+extern std::vector<std::string> content;
+extern std::string cmdlist;
+extern int ch;
+extern int max_y;
+extern int max_x;
+extern std::string strg;
