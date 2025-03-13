2784b46ece0669ffd0e852538d9f34db0a24e8d8
StaffMeeting <flintwong6@gmail.com>
2025-03-13 16:50:25 +0800

    found thing for patching bug inconvenient

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ int main(int argc, char* argv[]) {
         }
     }
     if (content.empty()) {
-        content.push_back("created new file.");
+        content.push_back(" ");
     }
     display();
     int ch;
